558f0a3bcecc808ac985301d47b50b3f151535cd
Giovani <giovanibossoni@hotmail.com>
2018-10-11 15:51:16 -0300

    Terminando Login

--- src/app/model/auth.service.ts
+++ src/app/model/auth.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { AuthData } from "./authData.model";
+import { Router } from "@angular/router";
 @Injectable({ providedIn: "root" })
 export class AuthService {
 
-    constructor(private http: HttpClient){
+    constructor(private http: HttpClient,private router: Router){
 
     }
 
     login(data: AuthData){
-        this.http.post("http://localhost:3001/auth", data).subscribe(response => {
+        this.http.post("http://localhost:3001/auth/login", data).subscribe(response => {
             console.log(response);
+            if(response["loginFlag"])
+                this.router.navigate(["/home"]);
         });
     }
 }
